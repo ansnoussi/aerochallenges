5640e51731f358359d5658c7b0f77e28537ea1c6
ansnoussi <anis0snoussi@gmail.com>
2019-04-01 16:01:04 +0100

    tempPage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
   constructor(props){
     super(props);
     Firebase.initializeApp(config.firebase);
-
+    this.firstTime = true ;
+    this.boardIsShown = true ;
     this.state = {
       players: []
     }
@@ -35,82 +36,92 @@ class App extends React.Component {
   componentDidMount() {
     this.getUserData();
   }
+
   
   componentDidUpdate(prevProps, prevState) {
     // check on previous state
     // only write when it's different with the new state
     if (prevState !== this.state) {
       this.writeUserData();
+      if(this.firstTime == true){
+        this.firstTime = false ;
+      }else{
+        console.log("NEW DATA ADDED");
+        this.boardIsShown = false ;
+        setTimeout(function(){
+          this.boardIsShown = true; 
+          this.forceUpdate(); 
+        }.bind(this), 3000);
+      }
     }
   }
 
   render() {
     const { players } = this.state;
-    const listPlayers = players.map(player =>
-        <div key={player.uid} className="card float-left" style={{width: '18rem', marginRight: '1rem'}}>
-        <div className="card-body">
-          <h5 className="card-title">{ player.name }</h5>
-          <p className="card-text">{ player.score }</p>
-          <button onClick={ () => this.removeData(player) } className="btn btn-link">Delete</button>
-          <button onClick={ () => this.updateData(player) } className="btn btn-link">Edit</button>
-        </div>
+    const listPLayerTable = players.sort((a, b) => {return((a.score - b.score) < 0)})
+    .map(player =>
+    <tr key={player.uid}>
+      <th scope="row">{player.name}</th>
+      <th>{player.score}</th>
+      <th>
+         <button onClick={ () => this.removeData(player) } className="btn btn-link">Delete</button>
+      </th>
+      <th>
+         <button onClick={ () => this.updateData(player) } className="btn btn-link">Edit</button>
+      </th>
+    </tr>
+    );
+
+    const showThings = (this.boardIsShown) ? 
+
+    <div className="container-fluid">
+    <div className="row">
+      <div className='col-xl-12'>
+        <h1 align="center">Airshow ScoreBoard</h1>
       </div>
-      );
-      const listPLayerTable = players.sort((a, b) => a.score < b.score)
-          .map(player =>
-          <tr key={player.uid}>
-            <th scope="row">{player.name}</th>
-            <th>{player.score}</th>
-            <th>
-               <button onClick={ () => this.removeData(player) } className="btn btn-link">Delete</button>
-            </th>
-            <th>
-               <button onClick={ () => this.updateData(player) } className="btn btn-link">Edit</button>
-            </th>
-          </tr>
-        );
+    </div>
+    <div className='row'>
+      <table className="table">
+      <thead>
+        <tr className="table-primary">
+          <th scope="col">Team Name</th>
+          <th scope="col">Score</th>
+          <th scope="col">function 1</th>
+          <th scope="col">function 2</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listPLayerTable}
+      </tbody>
+      </table>
+    </div>
+    <div className='row'>
+      <div className='col-xl-12'>
+        <h1>Add new player here</h1>
+        <form onSubmit={ this.handleSubmit }>
+          <div className="form-row">
+            <input type='hidden' ref='uid' />
+            <div className="form-group col-md-6">
+              <label>Name</label>
+              <input type="text" ref='name' className="form-control" placeholder="Name" />
+            </div>
+            <div className="form-group col-md-6">
+              <label>Score</label>
+              <input type="text" ref='score' className="form-control" placeholder="Score" />
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  :
+  <div><p>New player added</p></div>
+
+  ;
     return(
-      <div className="container-fluid">
-        <div className="row">
-          <div className='col-xl-12'>
-            <h1 align="center">Aerochallenge ScoreBoard</h1>
-          </div>
-        </div>
-        <div className='row'>
-          <table className="table">
-          <thead>
-            <tr className="table-primary">
-              <th scope="col">Team Name</th>
-              <th scope="col">Score</th>
-              <th scope="col">function 1</th>
-              <th scope="col">function 2</th>
-            </tr>
-          </thead>
-          <tbody>
-            {listPLayerTable}
-          </tbody>
-          </table>
-        </div>
-        <div className='row'>
-          <div className='col-xl-12'>
-            <h1>Add new player here</h1>
-            <form onSubmit={ this.handleSubmit }>
-              <div className="form-row">
-                <input type='hidden' ref='uid' />
-                <div className="form-group col-md-6">
-                  <label>Name</label>
-                  <input type="text" ref='name' className="form-control" placeholder="Name" />
-                </div>
-                <div className="form-group col-md-6">
-                  <label>Score</label>
-                  <input type="text" ref='score' className="form-control" placeholder="Score" />
-                </div>
-              </div>
-              <button type="submit" className="btn btn-primary">Save</button>
-            </form>
-          </div>
-        </div>
-      </div>
+    <div>{showThings}</div>  
     )
   }
   
