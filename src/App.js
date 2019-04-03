5ab247dc92ea2d61269fb84ce3003372afdfcfe8
ansnoussi <anis0snoussi@gmail.com>
2019-04-03 15:35:44 +0100

    show_player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Firebase from 'firebase';
 import config from './config';
+import NewPlayer from './components/NewPlayer';
 
 
 class App extends React.Component {
@@ -8,7 +9,9 @@ class App extends React.Component {
   constructor(props){
     super(props);
     Firebase.initializeApp(config.firebase);
-
+    this.firstTime = true ;
+    this.boardIsShown = false ;
+    this.lastPLayer = null ;
     this.state = {
       players: []
     }
@@ -35,82 +38,99 @@ class App extends React.Component {
   componentDidMount() {
     this.getUserData();
   }
+
   
   componentDidUpdate(prevProps, prevState) {
     // check on previous state
     // only write when it's different with the new state
     if (prevState !== this.state) {
       this.writeUserData();
+      let ref = Firebase.database().ref('/players');
+      ref.orderByChild('uid')
+      .startAt(Date.now())
+      .limitToLast(1)
+      .on('child_added', snapshot => {
+      this.lastPLayer = snapshot.val();
+    });
+      if(this.firstTime == true){
+        this.firstTime = false ;
+      }else{
+        console.log("NEW DATA ADDED");
+        this.boardIsShown = false ;
+        setTimeout(function(){
+          this.boardIsShown = true; 
+          this.forceUpdate(); 
+        }.bind(this), 5000);
+      }
     }
   }
 
   render() {
     const { players } = this.state;
-    const listPlayers = players.map(player =>
-        <div key={player.uid} className="card float-left" style={{width: '18rem', marginRight: '1rem'}}>
-        <div className="card-body">
-          <h5 className="card-title">{ player.name }</h5>
-          <p className="card-text">{ player.score }</p>
-          <button onClick={ () => this.removeData(player) } className="btn btn-link">Delete</button>
-          <button onClick={ () => this.updateData(player) } className="btn btn-link">Edit</button>
-        </div>
+    const listPLayerTable = players.sort((a, b) => {return((a.score - b.score) < 0)})
+    .map(player =>
+    <tr key={player.uid}>
+      <th scope="row">{player.name}</th>
+      <th>{player.score}</th>
+      <th>
+         <button onClick={ () => this.removeData(player) } className="btn btn-link">Delete</button>
+      </th>
+      <th>
+         <button onClick={ () => this.updateData(player) } className="btn btn-link">Edit</button>
+      </th>
+    </tr>
+    );
+
+    const showThings = (this.boardIsShown) ? 
+
+    <div className="container-fluid">
+    <div className="row">
+      <div className='col-xl-12'>
+        <h1 align="center">Airshow ScoreBoard</h1>
       </div>
-      );
-      const listPLayerTable = players.sort((a, b) => a.score < b.score)
-          .map(player =>
-          <tr key={player.uid}>
-            <th scope="row">{player.name}</th>
-            <th>{player.score}</th>
-            <th>
-               <button onClick={ () => this.removeData(player) } className="btn btn-link">Delete</button>
-            </th>
-            <th>
-               <button onClick={ () => this.updateData(player) } className="btn btn-link">Edit</button>
-            </th>
-          </tr>
-        );
+    </div>
+    <div className='row'>
+      <table className="table">
+      <thead>
+        <tr className="table-primary">
+          <th scope="col">Team Name</th>
+          <th scope="col">Score</th>
+          <th scope="col">function 1</th>
+          <th scope="col">function 2</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listPLayerTable}
+      </tbody>
+      </table>
+    </div>
+    <div className='row'>
+      <div className='col-xl-12'>
+        <h1>Add new player here</h1>
+        <form onSubmit={ this.handleSubmit }>
+          <div className="form-row">
+            <input type='hidden' ref='uid' />
+            <div className="form-group col-md-6">
+              <label>Name</label>
+              <input type="text" ref='name' className="form-control" placeholder="Name" />
+            </div>
+            <div className="form-group col-md-6">
+              <label>Score</label>
+              <input type="text" ref='score' className="form-control" placeholder="Score" />
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">Save</button>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  :
+  <div><NewPlayer player={this.lastPLayer} ></NewPlayer></div>
+
+  ;
     return(
-      <div className="container-fluid">
-        <div className="row">
-          <div className='col-xl-12'>
-            <h1 align="center">Aerochallenge ScoreBoard</h1>
-          </div>
-        </div>
-        <div className='row'>
-          <table className="table">
-          <thead>
-            <tr className="table-primary">
-              <th scope="col">Team Name</th>
-              <th scope="col">Score</th>
-              <th scope="col">function 1</th>
-              <th scope="col">function 2</th>
-            </tr>
-          </thead>
-          <tbody>
-            {listPLayerTable}
-          </tbody>
-          </table>
-        </div>
-        <div className='row'>
-          <div className='col-xl-12'>
-            <h1>Add new player here</h1>
-            <form onSubmit={ this.handleSubmit }>
-              <div className="form-row">
-                <input type='hidden' ref='uid' />
-                <div className="form-group col-md-6">
-                  <label>Name</label>
-                  <input type="text" ref='name' className="form-control" placeholder="Name" />
-                </div>
-                <div className="form-group col-md-6">
-                  <label>Score</label>
-                  <input type="text" ref='score' className="form-control" placeholder="Score" />
-                </div>
-              </div>
-              <button type="submit" className="btn btn-primary">Save</button>
-            </form>
-          </div>
-        </div>
-      </div>
+    <div>{showThings}</div>  
     )
   }
   
@@ -130,7 +150,7 @@ class App extends React.Component {
       this.setState({ players });
     }
     else if (name && score ) {
-      const uid = new Date().getTime().toString();
+      const uid = new Date().getTime();
       const { players } = this.state;
       players.push({ uid, name, score })
       this.setState({ players });
---
+++ src/components/NewPlayer.js
@@ -0,0 +1,404 @@
+import React from 'react';
+import { Text, Image, Button, Card } from './mini'
+import { Composition } from 'atomic-layout'
+
+
+const templateMobile = `
+  thumbnail
+  heading
+  actions
+`
+
+const templateTablet = `
+  thumbnail heading
+  thumbnail actions
+`
+
+
+
+  const QuitterZoneCard = ({ nbEssai }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail>
+              <Image src="../assets/sample.png" alt="zoneImage" />
+            </Thumbnail>
+            <Heading>
+              <h3>Zone de départ</h3>
+            {(nbEssai > 1) ? <Text small >echec du premier essai : -3 points</Text> : <div></div>}
+            </Heading>
+            <Actions flex align="flex-end">
+              {(nbEssai == 0) ? <Button wide style={{backgroundColor:'red'}} >0 points</Button> : <Button wide style={{backgroundColor:'green'}} >+ {(nbEssai > 1)? 2 : 5} points</Button>}
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+  const BoolCard = ({ title, bool, points, imageUrl }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail>
+              <Image src={imageUrl} alt={title} />
+            </Thumbnail>
+            <Heading>
+              <h3>{title}</h3>
+            </Heading>
+            <Actions flex align="flex-end">
+              {(bool == 0) ? <Button wide  style={{backgroundColor:'red'}} >0 points</Button> : <Button wide  style={{backgroundColor:'green'}} >+ {points} points</Button>}
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+  
+  const ColisionCard = ({ nbr }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail >
+              <Text   style={{fontSize: 100}}>{nbr}</Text>
+            </Thumbnail>
+            <Heading>
+              <h3>Collisions</h3>
+            </Heading>
+            <Actions flex align="flex-end">
+            {(nbr >0) ? <Button wide  style={{backgroundColor:'red'}} >- {nbr} points</Button> : <div></div>}
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+  const ZIGZAGCard = ({ colonnes }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail >
+             <Image src="../assets/sample.png" alt="zigzag img" />
+            </Thumbnail>
+            <Heading>
+              <h3>ZIGZAG</h3>
+              {<Text small >passer {colonnes} colonnes</Text>}
+            </Heading>
+            <Actions flex align="flex-end">
+            {(colonnes == 3)? <Button wide  style={{backgroundColor:'green'}} >+ 30 points</Button> : <div></div> }
+            {(colonnes == 2)? <Button wide  style={{backgroundColor:'green'}} >+ 15 points</Button> : <div></div> }
+            {(colonnes == 0)? <Button wide  style={{backgroundColor:'red'}} >0 points</Button> : <div></div> }
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+
+  const OqbaCard = ({ tours }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail >
+             <Image src="../assets/sample.png" alt="zigzag img" />
+            </Thumbnail>
+            <Heading>
+              <h3>Mosquée Oqba</h3>
+              {<Text small >effectuer {tours} tours</Text>}
+            </Heading>
+            <Actions flex align="flex-end">
+            {(tours == 3)? <Button wide  style={{backgroundColor:'green'}} >+ 45 points</Button> : <div></div> }
+            {(tours == 2)? <Button wide  style={{backgroundColor:'green'}} >+ 30 points</Button> : <div></div> }
+            {(tours == 1)? <Button wide  style={{backgroundColor:'green'}} >+ 15 points</Button> : <div></div> }
+            {(tours == 0)? <Button wide  style={{backgroundColor:'red'}} >0 points</Button> : <div></div> }
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+  const BassinCard = ({ bassin, niveau, duree, poids }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail >
+             <Image src="../assets/sample.png" alt="zigzag img" />
+            </Thumbnail>
+            <Heading>
+              <h3>Bassins aghlabides </h3>
+              {(bassin == 0) ? <div></div> : <div> {<Text small >stabiliser au dessus du {(bassin == 1) ? "grand" : "petit"} bassin au {(niveau == 0) ? "dessous" : "dessus"} du niveau du muraille de Sfax pendant {duree} seconds </Text>} </div>}
+            </Heading>
+            <Actions flex align="flex-end">
+            {(bassin == 0)? <Button wide  style={{backgroundColor:'red'}} >0 points</Button> : <Button wide  style={{backgroundColor:'green'}} >+ {duree * poids} points</Button> }
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+
+  const SfaxCard = ({ portail }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail >
+             <Image src="../assets/sample.png" alt="zigzag img" />
+            </Thumbnail>
+            <Heading>
+              <h3>Muraille de Sfax</h3>
+              {(portail == 0)? null : <div> {<Text small >traverser le {(portail == 1) ? "petit" : "grand"} portail</Text>} </div>}
+            </Heading>
+            <Actions flex align="flex-end">
+            {(portail == 2)? <Button wide  style={{backgroundColor:'green'}} >+ 30 points</Button> : <div></div> }
+            {(portail == 1)? <Button wide  style={{backgroundColor:'green'}} >+ 50 points</Button> : <div></div> }
+            {(portail == 0)? <Button wide  style={{backgroundColor:'red'}} >0 points</Button> : <div></div> }
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+
+
+  const AfricaCard = ({ temps, points }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail >
+             <Image src="../assets/sample.png" alt="zigzag img" />
+            </Thumbnail>
+            <Heading>
+              <h3>Muraille de Sfax</h3>
+              {(temps == 0)? null : <div> {<Text small >atterrir sur la zone d'arrivé dans {temps} seconds</Text>} </div>}
+            </Heading>
+            <Actions flex align="flex-end">
+            {(temps > 0)? <Button wide  style={{backgroundColor:'green'}} >+ {points} points</Button> : <div></div> }
+            {(temps == 0)? <Button wide  style={{backgroundColor:'red'}} >0 points</Button> : <div></div> }
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+  const HelicesCard = ({ protege }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail >
+             <Image src="../assets/sample.png" alt="zigzag img" />
+            </Thumbnail>
+            <Heading>
+              <h3>Protection des hélices</h3>
+              <Text small >les hélices {(protege == 0) ? "ne sont pas" : "sont"} protégées</Text>
+            </Heading>
+            <Actions flex align="flex-end">
+            {(protege == 0)? <Button wide  style={{backgroundColor:'red'}} >- 10 points</Button> : <Button wide  style={{backgroundColor:'green'}} >- 0 points</Button> }
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+  const DiametreCard = ({ diametre }) => (
+    <Card>
+      <Composition
+        template={templateMobile}
+        templateMd={templateTablet}
+        templateLg={templateMobile}
+        templateColsMdOnly="minmax(100px, 1fr) 1fr"
+        padding={15}
+        gutter={15}
+        gutterLg={25}
+      >
+        {({ Thumbnail, Heading, Actions }) => (
+          <>
+            <Thumbnail >
+             <Image src="../assets/sample.png" alt="zigzag img" />
+            </Thumbnail>
+            <Heading>
+              <h3>Diametre</h3>
+              <Text small >{diametre} mm</Text>
+            </Heading>
+            <Actions flex align="flex-end">
+            {(diametre <= 195 )? <Button wide  style={{backgroundColor:'green'}} >non homologués</Button> : <Button wide  style={{backgroundColor:'red'}} >- 50 points</Button> }
+            </Actions>
+          </>
+        )}
+      </Composition>
+    </Card>
+  )
+
+
+
+export default class NewPlayer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            name : "bestTEAM",
+            uid : 1554215647463,
+            score : 900,
+            details : {
+                quitterZone : 2,
+                fabriquerParParticpant : 1,
+                posterTechnique : 0,
+                collisions : 2,
+                zigzag : 3,
+                murailleDeSfax : 1,
+                hotelAfrica : 22.3,
+                OqbaIbnNafi : 0,
+                bassins : 2,
+                niveauStabilisation : 0,
+                dureeStabilization : 2,
+                diametre: 120,
+                helicesProtegees: 0,
+            }
+        };
+    }
+
+    calculerPoidsSecond(bassin , niveau){
+      if(bassin == 1){
+        if(niveau == 0){
+          return 8;
+        }else{
+          return 4;
+        }
+      }else{
+        if(niveau == 0){
+          return 5;
+        }else{
+          return 4;
+        }
+      }
+    }
+    
+    calculerHotelAfricaTemps(temps){
+      if(temps < 3600){
+        return 36;
+      }else{
+        return(36 - Math.round((temps - 180) / 5));
+      }
+    }
+
+    render(){
+        return(<div className="container">
+            <h1>{this.state.name}</h1>
+            <h1>score: {this.state.score}</h1>
+            <Composition
+            templateCols="repeat(auto-fit, 250px)"
+            templateColsMd="repeat(2, 1fr)"
+            templateColsLg="repeat(auto-fit, minmax(250px, 1fr))"
+            justifyContent="center"
+            gutter={15}
+            gutterLg={25}
+            >
+
+<BoolCard title="Fabriquer par le participant" bool={this.state.details.fabriquerParParticpant} points={100} imageUrl="../assets/sample.png" ></BoolCard>
+<BoolCard title="Poster technique" bool={this.state.details.posterTechnique} points={10} imageUrl="../assets/sample.png" ></BoolCard>
+<HelicesCard protege={this.state.details.helicesProtegees} ></HelicesCard>
+<DiametreCard diametre={this.state.details.diametre} ></DiametreCard>
+<QuitterZoneCard key={this.state.uid} nbEssai={this.state.details.quitterZone} ></QuitterZoneCard>
+<ZIGZAGCard colonnes={this.state.details.zigzag} ></ZIGZAGCard>
+<BassinCard bassin={this.state.details.bassins} niveau={this.state.details.niveauStabilisation} duree={this.state.details.dureeStabilization} poids={this.calculerPoidsSecond(this.state.details.bassins, this.state.details.niveauStabilisation)} ></BassinCard>
+<ColisionCard nbr={this.state.details.collisions} ></ColisionCard>
+<OqbaCard tours={this.state.details.OqbaIbnNafi} ></OqbaCard>
+<SfaxCard portail={this.state.details.murailleDeSfax} ></SfaxCard>
+<AfricaCard temps={this.state.details.hotelAfrica} points={this.calculerHotelAfricaTemps(this.state.details.hotelAfrica)} ></AfricaCard>
+
+
+
+
+            
+          </Composition>
+        </div>);
+    }
+}
+
